dac6540c702f0dd8ca3021d743580753ae3111a9
gdpettus <gabe.pettus@gmail.com>
2020-02-24 18:56:59 -0700

    working on input

--- client/src/pages/Habits.js
+++ client/src/pages/Habits.js
@@ -4,10 +4,13 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Input from "../components/Input";
+import DeleteBtn from "../components/DeleteBtn";
+import SubmitBtn from "../components/SubmitBtn";
 
 function Habits() {
-  const [habits, setHabits] = useState([]);
-  const [formObject, setFormObject] = useState([]);
+  const [habitState, setHabitState] = useState([]);
+  console.log("Habits func - habits",habitState);
+  const [formObject, setFormObject] = useState({})
 
   useEffect(() => {
     loadHabits()
@@ -16,10 +19,16 @@ function Habits() {
   function loadHabits() {
    API.getHabit()
     .then(res =>
-      setHabits(res.data)
+      setHabitState(res.data)
     ) 
     .catch(err => console.log("error in loadHabit", err));
   };
+
+  function deleteHabit(id) {
+    API.deleteHabit(id)
+    .then(res => loadHabits())
+    .catch(err => console.log(err));
+  }
 
   // grabs values on change and update onject
   function handleInputChange(e) {
@@ -28,42 +37,66 @@ function Habits() {
   };
 
   // takes object and calls save endpoint when form is submitted
-  // function handleFormSubmit(e) {
-    // e.preventDefault();
-    // may need to expand this logic out
-    // if (formObject.habitName) {
-      // API.saveHabit
-
-    // }
-  // }
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.habitName && true) {
+      API.saveHabit({
+        habitName: formObject.habitName,
+        weight: formObject.weight,
+      })
+        .then(res => loadHabits())
+        .catch(err => console.log(err));
+    }
+  };
 
   return (
     <Container fluid>
       <Row>
         <h1>Habits you have selected</h1>
         <form>
-          <Input
+          Enter Habit: <Input
             onChange={handleInputChange}
             name="habitName"
             placeholder="Habit (required)"
           />
+
+          Enter Weight of Habit<Input
+            onChange={handleInputChange}
+            name="weight"
+            placeholder="Weight (required)"
+          />
+          <SubmitBtn
+            disabled={!(formObject.habitName)}
+            onClick={handleFormSubmit}
+          >
+          </SubmitBtn>
         </form>
       </Row>
       <Row>
 
-      {habits.length ? (
+{console.log("habit.length",habitState.length)};
+
+      {habitState === undefined ? (
         <List>
-          <ListItem key={habits._id}>
-            <Link to={"habits/" + habits._id}>
-              <strong>
-                {habits.habitName}
-              </strong>
-            </Link>
-          </ListItem>
+
+          {habitState.map(e => (
+            <ListItem key={e._id}>
+              <List to={"/habits/" + e._id}>
+                <strong>
+                  {e.habitName}
+                </strong>
+              </List>
+              <DeleteBtn onClick={() => deleteHabit(e._id)} />
+            </ListItem>
+          ))}
         </List>
     ) : (
+<>
+      {console.log("habits",habitState)}
       <h3> No Resutls to Display</h3>
+</>
     )}
+      {console.log("habits",typeof habits)}
       </Row>
     </Container>
   );
